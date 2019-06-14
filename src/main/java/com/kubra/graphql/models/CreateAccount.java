fea94797b2630500aef6df82540887ac721908ac
Ryan Walker <ryan.walker@infusionsoft.com>
2019-06-14 12:22:27 -0700

    Rest controller

--- src/main/java/com/kubra/graphql/models/CreateAccount.java
+++ src/main/java/com/kubra/graphql/models/CreateAccount.java
@@ -1,11 +1,42 @@
 package com.kubra.graphql.models;
 
-import lombok.Data;
 
-@Data
 public class CreateAccount {
   private String externalAccountId;
   private String serviceAddressLine1;
   private String serviceAddressLine2;
   private AccountStatus accountStatus;
+
+
+  public String getExternalAccountId() {
+    return externalAccountId;
+  }
+
+  public void setExternalAccountId(String externalAccountId) {
+    this.externalAccountId = externalAccountId;
+  }
+
+  public String getServiceAddressLine1() {
+    return serviceAddressLine1;
+  }
+
+  public void setServiceAddressLine1(String serviceAddressLine1) {
+    this.serviceAddressLine1 = serviceAddressLine1;
+  }
+
+  public String getServiceAddressLine2() {
+    return serviceAddressLine2;
+  }
+
+  public void setServiceAddressLine2(String serviceAddressLine2) {
+    this.serviceAddressLine2 = serviceAddressLine2;
+  }
+
+  public AccountStatus getAccountStatus() {
+    return accountStatus;
+  }
+
+  public void setAccountStatus(AccountStatus accountStatus) {
+    this.accountStatus = accountStatus;
+  }
 }
--- src/main/java/com/kubra/rest/HelloWorldController.java
+++ src/main/java/com/kubra/rest/HelloWorldController.java
@@ -1,5 +1,26 @@
 package com.kubra.rest;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(
+    path = "/api/hello",
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE
+)
 public class HelloWorldController {
+
+  @GetMapping
+  public ResponseEntity hello() {
+    Map<String, String> body = new HashMap<>();
+    body.put("hello", "world");
+    return ResponseEntity.ok(body);
+  }
 
 }
